41059292e1390fce1aa307ce975c0ed97ef0a36c
julesdezf <jules.barbe@capessor.com>
2025-10-31 10:37:57 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CAApp from "./CAApp.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CAApp />
+  </React.StrictMode>
+);
